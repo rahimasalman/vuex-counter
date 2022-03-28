119bb1ea2ecbeb14732e594106d77d792554b669
rahimasalman <rehimesalman7@gmail.com>
2022-03-28 16:20:55 +0400

    Update Home page

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Counter />
+    <Buttons />
+    <CounterSquared />
+    <ColorCode />
+  </div>
+</template>
+
+<script>
+import Counter from "../components/Counter";
+import CounterSquared from "../components/CounterSquared";
+import ColorCode from "../components/ColorCode";
+import Buttons from "../components/Buttons";
+export default {
+  name: "Home",
+  components: {
+    Counter,
+    CounterSquared,
+    ColorCode,
+    Buttons,
+  },
+};
+</script>
+<style scoped>
+div {
+  margin-bottom: 20px;
+}
+</style>
